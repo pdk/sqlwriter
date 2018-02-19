9db4b11deee3bdd698d9d207067291e3066eba20
Patrick Kelly <patrick.kelly@zuora.com>
2018-02-18 20:19:04 -1000

    add example for ProcessRows

--- process_rows.go
+++ process_rows.go
@@ -25,3 +25,30 @@ func ProcessRows(db *sql.DB, query Statement, scanner RowHandler) (int, error) {
 
 	return counter, nil
 }
+
+// The following is a (non-tested) example of how to use the ProcessRows
+// function, above.
+
+type recordType struct {
+	Name    string
+	Address string
+}
+
+func queryRecords(db *sql.DB) ([]recordType, error) {
+
+	var accumulator []recordType
+	scanner := func(r *sql.Rows) error {
+		nextRecord := recordType{}
+		err := r.Scan(&nextRecord.Name, &nextRecord.Address)
+		accumulator = append(accumulator, nextRecord)
+		return err
+	}
+
+	query := Select(DBNames(recordType{})...).
+		From("latable").
+		Expr("name", GreaterThanOrEquals, "Bobby")
+
+	_, err := ProcessRows(db, query, scanner)
+
+	return accumulator, err
+}
